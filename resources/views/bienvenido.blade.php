1497976a49d83cf548ac2e2ee6050dd14db7ad13
Derling Obando <derling.obando88@gmail.com>
2019-02-26 18:46:45 -0600

    listo

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,155 @@
+
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>BIENVENIDO</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="/package/assets/css/main.css" />
+
+	</head>
+	<body class="landing is-preload">
+		<div id="page-wrapper">
+
+            <!-- Header -->
+				<header id="header" class="alt">
+					<h1><a href="/bienvenido">INICIO</a> -DerlingO Blog- </h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="/bienvenido">Home</a></li>
+							<li><a href="/eventos">Eventos</a></li>
+							<li><a href="/contacto">Contacto</a></li>
+							<li><a href="/elementos">Elementos</a></li>
+							<!--<li><a href="#" class="button">Sign Up</a></li>-->
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<h2>Bienvenidos</h2>
+					<p>Este blog esta destinado para compartir contenidos de Desarrollo y Diseños Web</p>
+					<ul class="actions special">
+						<li><a href="#" class="button primary">Sign Up</a></li>
+						<li><a href="#" class="button">Learn More</a></li>
+					</ul>
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+
+					<section class="box special">
+						<header class="major">
+							<h2>Introduciendo en el mundo del Diseño Web
+							<br />
+							compartiendo nuestras experiencias</h2>
+							<p> Toda la información estar ordenada por categorias dependiendo de la tecnologia
+                                que estes utilizando asi enontraras mas facilmente lo que necesitas.</p>
+						</header>
+						<span class="image featured"><img src="/package/assets/images/pic01.jpg" alt="" /></span>
+					</section>
+
+					<section class="box special features">
+						<div class="features-row">
+							<section>
+								<span class="icon major fa-bolt accent2"></span>
+								<h3>HTML5, CSS y JS</h3>
+								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+							</section>
+							<section>
+								<span class="icon major fa-area-chart accent3"></span>
+								<h3>Laravel y PHP</h3>
+								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+							</section>
+						</div>
+						<div class="features-row">
+							<section>
+								<span class="icon major fa-cloud accent4"></span>
+								<h3>Diseño Web seguro</h3>
+								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+							</section>
+							<section>
+								<span class="icon major fa-lock accent5"></span>
+								<h3>Django y Python</h3>
+								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+							</section>
+						</div>
+					</section>
+
+					<div class="row">
+						<div class="col-6 col-12-narrower">
+
+							<section class="box special">
+								<span class="image featured"><img src="/package/assets/images/pic02.jpg" alt="" /></span>
+								<h3>Sed lorem adipiscing</h3>
+								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+								<ul class="actions special">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+
+						</div>
+						<div class="col-6 col-12-narrower">
+
+							<section class="box special">
+								<span class="image featured"><img src="/package/assets/images/pic03.jpg" alt="" /></span>
+								<h3>Accumsan integer</h3>
+								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+								<ul class="actions special">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+
+						</div>
+					</div>
+
+				</section>
+
+			<!-- CTA -->
+				<section id="cta">
+
+					<h2>Inscribete para recibir las últimas noticias</h2>
+					<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc.</p>
+
+					<form>
+						<div class="row gtr-50 gtr-uniform">
+							<div class="col-8 col-12-mobilep">
+								<input type="email" name="email" id="email" placeholder="Email Address" />
+							</div>
+							<div class="col-4 col-12-mobilep">
+								<input type="submit" value="Sign Up" class="fit" />
+							</div>
+						</div>
+					</form>
+
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. Todos los derechos.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="/package/assets/js/jquery.min.js"></script>
+			<script src="/package/assets/js/jquery.dropotron.min.js"></script>
+			<script src="/package/assets/js/jquery.scrollex.min.js"></script>
+			<script src="/package/assets/js/browser.min.js"></script>
+			<script src="/package/assets/js/breakpoints.min.js"></script>
+			<script src="/package/assets/js/util.js"></script>
+			<script src="/package/assets/js/main.js"></script>
+
+	</body>
+</html>
